d712cacea550f4a304e081aa4ddbb5fd583b1981
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 15:30:54 +0100

    registered new url routes for carts cartitems and orders

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
-from . import views
+from .views import *
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 
 router = routers.DefaultRouter()
-router.register('products', views.ProductViewSet, basename='products')
-router.register('collections', views.CollectionViewSet)
+router.register('products', ProductViewSet, basename='products')
+router.register('collections', CollectionViewSet)
+router.register('carts', CartViewSet, basename='cart')
+router.register('cart-items', CartItemViewSet, basename='cart-item')
+router.register('orders', OrderViewSet, basename='order')
 
 
 urlpatterns = [
