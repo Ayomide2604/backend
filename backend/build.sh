80e08390b4875be6ecc2486e6af643cf2e8015b4
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 13:50:18 +0100

    Updated build file to add makemigrations command

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,14 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install dependencies
 pip install -r requirements.txt
 
-# Convert static asset files
-python manage.py collectstatic --no-input
+# Make migrations (create migration files if needed)
+python manage.py makemigrations
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --no-input
