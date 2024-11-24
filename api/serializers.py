e2c49c288c7b5b72c083683f7ae5417bbd303c97
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 15:24:56 +0100

    Ceated Cart Serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Product, Collection
+from .models import Product, Collection, Cart
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -18,3 +18,22 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'collection', 'price']
+
+
+
+from rest_framework import serializers
+
+class CartSerializer(serializers.ModelSerializer):
+    total_price = serializers.SerializerMethodField()
+    cart_items = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'created_at', 'total_price', 'cart_items']
+
+    def get_total_price(self, obj):
+        return obj.total_price()
+
+    def get_cart_items(self, obj):
+        from .serializers import CartItemSerializer
+        return CartItemSerializer(obj.cart_items.all(), many=True).data
