ff6a8c97a2b1fad297033d3ebbddefee586dd023
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 13:58:31 +0100

    Added the collection and product models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    collection = models.ForeignKey(
+        Collection, on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['-date_created']
