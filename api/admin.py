3bd864b63645503798b48ffd8c1dd9b9a61a133c
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 14:00:11 +0100

    registered product and collectionmodels in admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Product, Collection
 # Register your models here.
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'collection')
+
+
+@admin.register(Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
