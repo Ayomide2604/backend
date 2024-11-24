c9dd4658e7d3e6bb2c2cb8f56ba546610886d08b
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 14:20:30 +0100

    Updated Rest Framework Jwt Settings

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
+
 router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet, basename='products')
 router.register('collections', views.CollectionViewSet)
@@ -8,5 +11,7 @@ router.register('collections', views.CollectionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
