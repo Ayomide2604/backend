b9c1141bc46ac09a27ed8029f0db9032841a9e6a
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 14:05:50 +0100

    Created Views for product and collection and registered their urls

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+import .views
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet, basename='products')
+router.register('collections', views.CollectionViewSet)
+
 
 urlpatterns = [
+    path('api/', include(router.urls)),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
+from .models import Collection, Product
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from . serializers import CollectionSerializer, ProductSerializer
+
 
 # Create your views here.
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+    permission_classes = [AllowAny]
+
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs['pk']):
+            return Response({'error': 'Collection cannot be deleted because it includes one or more products.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().destroy(request, *args, **kwargs)
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [AllowAny]
