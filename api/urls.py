4bf95b7fc68e04f18a3be657f4612b5c5702e379
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-24 18:35:21 +0100

    added media settings for urls

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import *
 from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
@@ -18,4 +20,4 @@ urlpatterns = [
         {'get': 'retrieve', 'delete': 'destroy'}), name='cart-detail'),
     path('order/<uuid:pk>/', OrderViewSet.as_view(
         {'get': 'retrieve', 'delete': 'destroy'}), name='order-detail'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
