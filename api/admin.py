f69bea42112285a3759d8637ba57c8e226dd0f2b
Ayomide2604 <106402640+Ayomide2604@users.noreply.github.com>
2024-11-27 07:21:01 +0100

    added orders to admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,18 +1,24 @@
 from django.contrib import admin
-from .models import Product, Collection, Cart
+from .models import Product, Collection, Cart, Order, OrderItem
 # Register your models here.
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'collection')
+    list_display = ['id', 'name', 'price', 'collection']
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('id',)
+    list_display = ['id']
 
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ['id', 'name']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'payment_status']
+    list_per_page = 10
